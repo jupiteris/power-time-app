00549d9573b448e4d673ba8e3f8f09dd6d02ab02
jupiteris <jupiter.programmer@gmail.com>
2023-03-24 13:31:46 -0400

    prob added

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-console.log(document.querySelector('.visual').clientWidth)
-
 const visualWindow = document.querySelector('.visual');
+const ellipse = document.querySelector('.ellipse');
+const offsetLine = document.querySelector('.offset-line');
+const probImg = document.querySelector('.prob-img');
+
+let length = 200; // should be less than parent width
+let height = 120;
+let offset = 10;
+
 visualWindow.style.height = visualWindow.clientWidth + "px";
+ellipse.style.width = length + 'px';
+ellipse.style.height = height + 'px';
+offsetLine.style.width = offset + 'px';
+probImg.style.marginRight = offset + 'px';
