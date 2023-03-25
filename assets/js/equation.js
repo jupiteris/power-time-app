21202bca5cb23ffbab2ad196529398919afa9e03
jupiteris <jupiter.programmer@gmail.com>
2023-03-25 07:07:47 -0400

    basic done

---
+++ assets/js/equation.js
@@ -0,0 +1,362 @@
+const Calculations = [
+    {
+        "Probe": "1P",
+        "Tissue": "LV",
+        "Dimension": "Length",
+        "Variable 1": 0.897,
+        "Variable 2": 0.039460,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "1P",
+        "Tissue": "LV",
+        "Dimension": "Height",
+        "Variable 1": 0.7691,
+        "Variable 2": 0.02011,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "1P",
+        "Tissue": "LV",
+        "Dimension": "Offset",
+        "Variable 1": 0.1337,
+        "Variable 2": 0.004165,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "1P",
+        "Tissue": "KD",
+        "Dimension": "Length",
+        "Variable 1": 0.8762,
+        "Variable 2": 0.03805,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "1P",
+        "Tissue": "KD",
+        "Dimension": "Height",
+        "Variable 1": 0.6996,
+        "Variable 2": 0.0223,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "1P",
+        "Tissue": "KD",
+        "Dimension": "Offset",
+        "Variable 1": 0.1571,
+        "Variable 2": 0.00378,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "1P",
+        "Tissue": "LG",
+        "Dimension": "Length",
+        "Variable 1": 0.6782,
+        "Variable 2": 0.03845,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "1P",
+        "Tissue": "LG",
+        "Dimension": "Height",
+        "Variable 1": 0.4649,
+        "Variable 2": 0.01847,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "1P",
+        "Tissue": "LG",
+        "Dimension": "Offset",
+        "Variable 1": 0.068,
+        "Variable 2": 0.003762,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "2P",
+        "Tissue": "LV",
+        "Dimension": "Length",
+        "Variable 1": 0.8031,
+        "Variable 2": 0.03982,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "2P",
+        "Tissue": "LV",
+        "Dimension": "Height",
+        "Variable 1": 0.7686,
+        "Variable 2": 0.02157,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "2P",
+        "Tissue": "LV",
+        "Dimension": "Offset",
+        "Variable 1": 0.1067,
+        "Variable 2": 0.004139,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "2P",
+        "Tissue": "KD",
+        "Dimension": "Length",
+        "Variable 1": 0.7939,
+        "Variable 2": 0.03755,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "2P",
+        "Tissue": "KD",
+        "Dimension": "Height",
+        "Variable 1": 0.6638,
+        "Variable 2": 0.02194,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "2P",
+        "Tissue": "KD",
+        "Dimension": "Offset",
+        "Variable 1": 0.1617,
+        "Variable 2": 0.00315,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "3L",
+        "Tissue": "LV",
+        "Dimension": "Length",
+        "Variable 1": 1.1177,
+        "Variable 2": 0.02654,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "3L",
+        "Tissue": "LV",
+        "Dimension": "Height",
+        "Variable 1": 0.9341,
+        "Variable 2": 0.012288,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "3L",
+        "Tissue": "LV",
+        "Dimension": "Offset",
+        "Variable 1": 0.413,
+        "Variable 2": 0.010584,
+        "Variable 3": -2
+    },
+    {
+        "Probe": "3L",
+        "Tissue": "KD",
+        "Dimension": "Length",
+        "Variable 1": 1.1582,
+        "Variable 2": 0.02351,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "3L",
+        "Tissue": "KD",
+        "Dimension": "Height",
+        "Variable 1": 0.8629,
+        "Variable 2": 0.012213,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "3L",
+        "Tissue": "KD",
+        "Dimension": "Offset",
+        "Variable 1": 0.6185,
+        "Variable 2": 0.00729,
+        "Variable 3": -2
+    },
+    {
+        "Probe": "3L",
+        "Tissue": "LG",
+        "Dimension": "Length",
+        "Variable 1": 1.1377,
+        "Variable 2": 0.03103,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "3L",
+        "Tissue": "LG",
+        "Dimension": "Height",
+        "Variable 1": 0.7469,
+        "Variable 2": 0.01385,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "3L",
+        "Tissue": "LG",
+        "Dimension": "Offset",
+        "Variable 1": 0.39,
+        "Variable 2": 0.00995,
+        "Variable 3": -2
+    },
+    {
+        "Probe": "4L",
+        "Tissue": "LV",
+        "Dimension": "Length",
+        "Variable 1": 1.1896,
+        "Variable 2": 0.024405,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "4L",
+        "Tissue": "LV",
+        "Dimension": "Height",
+        "Variable 1": 0.916,
+        "Variable 2": 0.01339,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "4L",
+        "Tissue": "LV",
+        "Dimension": "Offset",
+        "Variable 1": 0.4462,
+        "Variable 2": 0.00832,
+        "Variable 3": -2
+    },
+    {
+        "Probe": "4L",
+        "Tissue": "KD",
+        "Dimension": "Length",
+        "Variable 1": 1.0139,
+        "Variable 2": 0.02254,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "4L",
+        "Tissue": "KD",
+        "Dimension": "Height",
+        "Variable 1": 0.7766,
+        "Variable 2": 0.01315,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "4L",
+        "Tissue": "KD",
+        "Dimension": "Offset",
+        "Variable 1": 0.4118,
+        "Variable 2": 0.01012,
+        "Variable 3": -2
+    },
+    {
+        "Probe": "5S",
+        "Tissue": "LV",
+        "Dimension": "Length",
+        "Variable 1": 1.1861,
+        "Variable 2": 0.02564,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "5S",
+        "Tissue": "LV",
+        "Dimension": "Height",
+        "Variable 1": 0.9224,
+        "Variable 2": 0.015121,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "5S",
+        "Tissue": "LV",
+        "Dimension": "Offset",
+        "Variable 1": 0.4693,
+        "Variable 2": 0.00853,
+        "Variable 3": -2
+    },
+    {
+        "Probe": "5S",
+        "Tissue": "KD",
+        "Dimension": "Length",
+        "Variable 1": 1.1678,
+        "Variable 2": 0.02365,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "5S",
+        "Tissue": "KD",
+        "Dimension": "Height",
+        "Variable 1": 0.7901,
+        "Variable 2": 0.013141,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "5S",
+        "Tissue": "KD",
+        "Dimension": "Offset",
+        "Variable 1": 0.5624,
+        "Variable 2": 0.00684,
+        "Variable 3": -2
+    },
+    {
+        "Probe": "6L",
+        "Tissue": "LG",
+        "Dimension": "Length",
+        "Variable 1": 1.181,
+        "Variable 2": 0.02963,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "6L",
+        "Tissue": "LG",
+        "Dimension": "Height",
+        "Variable 1": 0.7403,
+        "Variable 2": 0.01308,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "6L",
+        "Tissue": "LG",
+        "Dimension": "Offset",
+        "Variable 1": 0.4313,
+        "Variable 2": 0.00743,
+        "Variable 3": -2
+    },
+    {
+        "Probe": "7P",
+        "Tissue": "LV",
+        "Dimension": "Length",
+        "Variable 1": 0.8728,
+        "Variable 2": 0.03245,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "7P",
+        "Tissue": "LV",
+        "Dimension": "Height",
+        "Variable 1": 0.8085,
+        "Variable 2": 0.01971,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "7P",
+        "Tissue": "LV",
+        "Dimension": "Offset",
+        "Variable 1": 0.1087,
+        "Variable 2": 0.003783,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "7P",
+        "Tissue": "KD",
+        "Dimension": "Length",
+        "Variable 1": 0.7903,
+        "Variable 2": 0.0313,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "7P",
+        "Tissue": "KD",
+        "Dimension": "Height",
+        "Variable 1": 0.6347,
+        "Variable 2": 0.01775,
+        "Variable 3": 0
+    },
+    {
+        "Probe": "7P",
+        "Tissue": "KD",
+        "Dimension": "Offset",
+        "Variable 1": 0.1003,
+        "Variable 2": 0.003666,
+        "Variable 3": 0
+    }
+]
---
+++ assets/js/script.js
@@ -0,0 +1,150 @@
+const visualDom = document.querySelector('.visual');
+const ellipseDom = document.querySelector('.ellipse');
+const offsetLine = document.querySelector('.offset-line');
+const probImg = document.querySelector('.probe-img');
+const infoModal = document.getElementById('info-modal');
+const tissueOptions = document.querySelectorAll('.tissues button');
+const probeQuantityOptions = document.querySelectorAll('.probe-quantity button');
+const probeSelectArrow = document.querySelector('.probe .arrow');
+
+// default values
+let tissue = 'LV';
+let probe = '1P';
+let time = 5;
+let power = 60;
+let probeQuantity = 1;
+let probeOpen = false;
+
+// static values
+const MIN_TIME = 1;
+const MAX_TIME = 5;
+const MIN_POWER = 35;
+const MAX_POWER = 60;
+const TIME_STEP = 1;
+const POWER_STEP = 5;
+const THRESHOLD = 4;
+
+// modal
+function openInfoModal() {
+    infoModal.style.display = "block";
+}
+
+function onCloseModal() {
+    infoModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == infoModal) {
+        infoModal.style.display = "none";
+    }
+}
+
+// mount event
+window.onload = function () {
+    visualDom.style.height = visualDom.clientWidth + "px";
+    renderEllipse();
+    renderTime();
+    renderPower();
+}
+
+function unselectAll(options) {
+    for (let i = 0; i < options.length; i++) {
+        options[i].className = "";
+    }
+}
+
+tissueOptions.forEach(option => {
+    option.addEventListener('click', function () {
+        unselectAll(tissueOptions);
+        option.className = "clicked";
+        tissue = option.getAttribute('value');
+        renderEllipse();
+    });
+});
+
+probeQuantityOptions.forEach(option => {
+    option.addEventListener('click', function () {
+        unselectAll(probeQuantityOptions);
+        option.className = "clicked";
+        probeQuantity = option.getAttribute('value')
+    });
+});
+
+function renderTime() {
+    document.querySelector('.time .value').innerHTML = time + ':00';
+    document.querySelector('.time .step .up').disabled = time === MAX_TIME
+    document.querySelector('.time .step .down').disabled = time === MIN_TIME
+}
+
+function renderPower() {
+    document.querySelector('.power .value').innerHTML = power;
+    document.querySelector('.power .step .up').disabled = power === MAX_POWER
+    document.querySelector('.power .step .down').disabled = power === MIN_POWER
+}
+
+function changeTime(direction) {
+    time += TIME_STEP * direction;
+    renderTime();
+    renderEllipse()
+}
+
+function changePower(direction) {
+    power += POWER_STEP * direction
+    renderPower();
+    renderEllipse()
+}
+
+// probe type selector
+function changeProbe(e) {
+    probe = e.value
+    e.blur()
+    renderEllipse()
+}
+
+function focusProbe() {
+    probeSelectArrow.classList.add('up')
+    probeSelectArrow.classList.remove('down')
+}
+
+function blurProbe() {
+    probeSelectArrow.classList.add('down')
+    probeSelectArrow.classList.remove('up')
+}
+
+function renderEllipse() {
+    let Dimensions = { Length: 200, Height: 150, Offset: null };
+    const equations = Calculations.filter(({ Probe, Tissue }) => Probe === probe && Tissue === tissue)
+
+    probImg.setAttribute('src', `assets/img/probs/${probe}.png`)
+
+    if (equations.length === 0) {
+        ellipseDom.style.display = "none"
+    }
+    else {
+        ellipseDom.style.display = "block"
+        equations.forEach(e => {
+            Dimensions[e.Dimension] = e['Variable 1'] * Math.log(time) + e['Variable 2'] * power + e['Variable 3']
+        })
+
+        ellipseDom.querySelector('.length-line div').innerHTML = Dimensions.Length.toFixed(1) + 'cm'
+        ellipseDom.querySelector('.height-line div').innerHTML = Dimensions.Height.toFixed(1) + 'cm'
+        ellipseDom.querySelector('.offset-line div').innerHTML = Math.abs(Dimensions.Offset).toFixed(1) + 'cm'
+
+        ellipseDom.style.width = (visualDom.clientWidth * Dimensions.Length / THRESHOLD) + 'px';
+        ellipseDom.style.height = (visualDom.clientWidth * Dimensions.Height / THRESHOLD) + 'px';
+        offsetLine.style.width = (visualDom.clientWidth * Math.abs(Dimensions.Offset) / THRESHOLD) + 'px';
+        probImg.style.marginRight = (visualDom.clientWidth * Dimensions.Offset / THRESHOLD) + 'px';
+
+        if (Dimensions.Offset < 0) {
+            ellipseDom.querySelector('.offset-line').style.right = (visualDom.clientWidth * Dimensions.Offset / THRESHOLD) + 'px';
+            ellipseDom.querySelector('.offset-line').style.bottom = '-8px';
+            ellipseDom.querySelector('.height-line div').style.marginTop = '-16px';
+        }
+        else {
+            ellipseDom.querySelector('.offset-line').style.right = 0;
+            ellipseDom.querySelector('.offset-line').style.bottom = 0;
+            ellipseDom.querySelector('.height-line div').style.marginTop = 0;
+        }
+    }
+}
