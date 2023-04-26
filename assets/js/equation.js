c26f79c272afd453ab3da779b11e2e0dae0f1624
jupiteris <jupiter.programmer@gmail.com>
2023-04-26 12:05:55 -0700

    style fix

--- assets/js/equation.js
+++ assets/js/equation.js
@@ -1,362 +1,460 @@
-const Calculations = [
+const CALCULATIONS = [
     {
-        "Probe": "1P",
-        "Tissue": "LV",
+        "Probe": "PR",
+        "Tissue": "Liver",
         "Dimension": "Length",
         "Variable 1": 0.897,
         "Variable 2": 0.039460,
         "Variable 3": 0
     },
     {
-        "Probe": "1P",
-        "Tissue": "LV",
+        "Probe": "PR",
+        "Tissue": "Liver",
         "Dimension": "Height",
         "Variable 1": 0.7691,
         "Variable 2": 0.02011,
         "Variable 3": 0
     },
     {
-        "Probe": "1P",
-        "Tissue": "LV",
+        "Probe": "PR",
+        "Tissue": "Liver",
         "Dimension": "Offset",
         "Variable 1": 0.1337,
         "Variable 2": 0.004165,
         "Variable 3": 0
     },
     {
-        "Probe": "1P",
-        "Tissue": "KD",
+        "Probe": "PR",
+        "Tissue": "Kidney",
         "Dimension": "Length",
         "Variable 1": 0.8762,
         "Variable 2": 0.03805,
         "Variable 3": 0
     },
     {
-        "Probe": "1P",
-        "Tissue": "KD",
+        "Probe": "PR",
+        "Tissue": "Kidney",
         "Dimension": "Height",
         "Variable 1": 0.6996,
         "Variable 2": 0.0223,
         "Variable 3": 0
     },
     {
-        "Probe": "1P",
-        "Tissue": "KD",
+        "Probe": "PR",
+        "Tissue": "Kidney",
         "Dimension": "Offset",
         "Variable 1": 0.1571,
         "Variable 2": 0.00378,
         "Variable 3": 0
     },
     {
-        "Probe": "1P",
-        "Tissue": "LG",
+        "Probe": "PR",
+        "Tissue": "Lung",
         "Dimension": "Length",
         "Variable 1": 0.6782,
         "Variable 2": 0.03845,
         "Variable 3": 0
     },
     {
-        "Probe": "1P",
-        "Tissue": "LG",
+        "Probe": "PR",
+        "Tissue": "Lung",
         "Dimension": "Height",
         "Variable 1": 0.4649,
         "Variable 2": 0.01847,
         "Variable 3": 0
     },
     {
-        "Probe": "1P",
-        "Tissue": "LG",
+        "Probe": "PR",
+        "Tissue": "Lung",
         "Dimension": "Offset",
         "Variable 1": 0.068,
         "Variable 2": 0.003762,
         "Variable 3": 0
     },
     {
-        "Probe": "2P",
-        "Tissue": "LV",
+        "Probe": "PR-XT",
+        "Tissue": "Liver",
         "Dimension": "Length",
         "Variable 1": 0.8031,
         "Variable 2": 0.03982,
         "Variable 3": 0
     },
     {
-        "Probe": "2P",
-        "Tissue": "LV",
+        "Probe": "PR-XT",
+        "Tissue": "Liver",
         "Dimension": "Height",
         "Variable 1": 0.7686,
         "Variable 2": 0.02157,
         "Variable 3": 0
     },
     {
-        "Probe": "2P",
-        "Tissue": "LV",
+        "Probe": "PR-XT",
+        "Tissue": "Liver",
         "Dimension": "Offset",
         "Variable 1": 0.1067,
         "Variable 2": 0.004139,
         "Variable 3": 0
     },
     {
-        "Probe": "2P",
-        "Tissue": "KD",
+        "Probe": "PR-XT",
+        "Tissue": "Kidney",
         "Dimension": "Length",
         "Variable 1": 0.7939,
         "Variable 2": 0.03755,
         "Variable 3": 0
     },
     {
-        "Probe": "2P",
-        "Tissue": "KD",
+        "Probe": "PR-XT",
+        "Tissue": "Kidney",
         "Dimension": "Height",
         "Variable 1": 0.6638,
         "Variable 2": 0.02194,
         "Variable 3": 0
     },
     {
-        "Probe": "2P",
-        "Tissue": "KD",
+        "Probe": "PR-XT",
+        "Tissue": "Kidney",
         "Dimension": "Offset",
         "Variable 1": 0.1617,
         "Variable 2": 0.00315,
         "Variable 3": 0
     },
     {
-        "Probe": "3L",
-        "Tissue": "LV",
+        "Probe": "LK",
+        "Tissue": "Liver",
         "Dimension": "Length",
         "Variable 1": 1.1177,
         "Variable 2": 0.02654,
         "Variable 3": 0
     },
     {
-        "Probe": "3L",
-        "Tissue": "LV",
+        "Probe": "LK",
+        "Tissue": "Liver",
         "Dimension": "Height",
         "Variable 1": 0.9341,
         "Variable 2": 0.012288,
         "Variable 3": 0
     },
     {
-        "Probe": "3L",
-        "Tissue": "LV",
+        "Probe": "LK",
+        "Tissue": "Liver",
         "Dimension": "Offset",
         "Variable 1": 0.413,
         "Variable 2": 0.010584,
         "Variable 3": -2
     },
     {
-        "Probe": "3L",
-        "Tissue": "KD",
+        "Probe": "LK",
+        "Tissue": "Kidney",
         "Dimension": "Length",
         "Variable 1": 1.1582,
         "Variable 2": 0.02351,
         "Variable 3": 0
     },
     {
-        "Probe": "3L",
-        "Tissue": "KD",
+        "Probe": "LK",
+        "Tissue": "Kidney",
         "Dimension": "Height",
         "Variable 1": 0.8629,
         "Variable 2": 0.012213,
         "Variable 3": 0
     },
     {
-        "Probe": "3L",
-        "Tissue": "KD",
+        "Probe": "LK",
+        "Tissue": "Kidney",
         "Dimension": "Offset",
         "Variable 1": 0.6185,
         "Variable 2": 0.00729,
         "Variable 3": -2
     },
     {
-        "Probe": "3L",
-        "Tissue": "LG",
+        "Probe": "LK",
+        "Tissue": "Lung",
         "Dimension": "Length",
         "Variable 1": 1.1377,
         "Variable 2": 0.03103,
         "Variable 3": 0
     },
     {
-        "Probe": "3L",
-        "Tissue": "LG",
+        "Probe": "LK",
+        "Tissue": "Lung",
         "Dimension": "Height",
         "Variable 1": 0.7469,
         "Variable 2": 0.01385,
         "Variable 3": 0
     },
     {
-        "Probe": "3L",
-        "Tissue": "LG",
+        "Probe": "LK",
+        "Tissue": "Lung",
         "Dimension": "Offset",
         "Variable 1": 0.39,
         "Variable 2": 0.00995,
         "Variable 3": -2
     },
     {
-        "Probe": "4L",
-        "Tissue": "LV",
+        "Probe": "LK-XT",
+        "Tissue": "Liver",
         "Dimension": "Length",
         "Variable 1": 1.1896,
         "Variable 2": 0.024405,
         "Variable 3": 0
     },
     {
-        "Probe": "4L",
-        "Tissue": "LV",
+        "Probe": "LK-XT",
+        "Tissue": "Liver",
         "Dimension": "Height",
         "Variable 1": 0.916,
         "Variable 2": 0.01339,
         "Variable 3": 0
     },
     {
-        "Probe": "4L",
-        "Tissue": "LV",
+        "Probe": "LK-XT",
+        "Tissue": "Liver",
         "Dimension": "Offset",
         "Variable 1": 0.4462,
         "Variable 2": 0.00832,
         "Variable 3": -2
     },
     {
-        "Probe": "4L",
-        "Tissue": "KD",
+        "Probe": "LK-XT",
+        "Tissue": "Kidney",
         "Dimension": "Length",
         "Variable 1": 1.0139,
         "Variable 2": 0.02254,
         "Variable 3": 0
     },
     {
-        "Probe": "4L",
-        "Tissue": "KD",
+        "Probe": "LK-XT",
+        "Tissue": "Kidney",
         "Dimension": "Height",
         "Variable 1": 0.7766,
         "Variable 2": 0.01315,
         "Variable 3": 0
     },
     {
-        "Probe": "4L",
-        "Tissue": "KD",
+        "Probe": "LK-XT",
+        "Tissue": "Kidney",
         "Dimension": "Offset",
         "Variable 1": 0.4118,
         "Variable 2": 0.01012,
         "Variable 3": -2
     },
     {
-        "Probe": "5S",
-        "Tissue": "LV",
+        "Probe": "SR",
+        "Tissue": "Liver",
         "Dimension": "Length",
         "Variable 1": 1.1861,
         "Variable 2": 0.02564,
         "Variable 3": 0
     },
     {
-        "Probe": "5S",
-        "Tissue": "LV",
+        "Probe": "SR",
+        "Tissue": "Liver",
         "Dimension": "Height",
         "Variable 1": 0.9224,
         "Variable 2": 0.015121,
         "Variable 3": 0
     },
     {
-        "Probe": "5S",
-        "Tissue": "LV",
+        "Probe": "SR",
+        "Tissue": "Liver",
         "Dimension": "Offset",
         "Variable 1": 0.4693,
         "Variable 2": 0.00853,
         "Variable 3": -2
     },
     {
-        "Probe": "5S",
-        "Tissue": "KD",
+        "Probe": "SR",
+        "Tissue": "Kidney",
         "Dimension": "Length",
         "Variable 1": 1.1678,
         "Variable 2": 0.02365,
         "Variable 3": 0
     },
     {
-        "Probe": "5S",
-        "Tissue": "KD",
+        "Probe": "SR",
+        "Tissue": "Kidney",
         "Dimension": "Height",
         "Variable 1": 0.7901,
         "Variable 2": 0.013141,
         "Variable 3": 0
     },
     {
-        "Probe": "5S",
-        "Tissue": "KD",
+        "Probe": "SR",
+        "Tissue": "Kidney",
         "Dimension": "Offset",
         "Variable 1": 0.5624,
         "Variable 2": 0.00684,
         "Variable 3": -2
     },
     {
-        "Probe": "6L",
-        "Tissue": "LG",
+        "Probe": "LN",
+        "Tissue": "Lung",
         "Dimension": "Length",
         "Variable 1": 1.181,
         "Variable 2": 0.02963,
         "Variable 3": 0
     },
     {
-        "Probe": "6L",
-        "Tissue": "LG",
+        "Probe": "LN",
+        "Tissue": "Lung",
         "Dimension": "Height",
         "Variable 1": 0.7403,
         "Variable 2": 0.01308,
         "Variable 3": 0
     },
     {
-        "Probe": "6L",
-        "Tissue": "LG",
+        "Probe": "LN",
+        "Tissue": "Lung",
         "Dimension": "Offset",
         "Variable 1": 0.4313,
         "Variable 2": 0.00743,
         "Variable 3": -2
     },
     {
-        "Probe": "7P",
-        "Tissue": "LV",
+        "Probe": "PRS15",
+        "Tissue": "Liver",
         "Dimension": "Length",
         "Variable 1": 0.8728,
         "Variable 2": 0.03245,
         "Variable 3": 0
     },
     {
-        "Probe": "7P",
-        "Tissue": "LV",
+        "Probe": "PRS15",
+        "Tissue": "Liver",
         "Dimension": "Height",
         "Variable 1": 0.8085,
         "Variable 2": 0.01971,
         "Variable 3": 0
     },
     {
-        "Probe": "7P",
-        "Tissue": "LV",
+        "Probe": "PRS15",
+        "Tissue": "Liver",
         "Dimension": "Offset",
         "Variable 1": 0.1087,
         "Variable 2": 0.003783,
         "Variable 3": 0
     },
     {
-        "Probe": "7P",
-        "Tissue": "KD",
+        "Probe": "PRS15",
+        "Tissue": "Kidney",
         "Dimension": "Length",
         "Variable 1": 0.7903,
         "Variable 2": 0.0313,
         "Variable 3": 0
     },
     {
-        "Probe": "7P",
-        "Tissue": "KD",
+        "Probe": "PRS15",
+        "Tissue": "Kidney",
         "Dimension": "Height",
         "Variable 1": 0.6347,
         "Variable 2": 0.01775,
         "Variable 3": 0
     },
     {
-        "Probe": "7P",
-        "Tissue": "KD",
+        "Probe": "PRS15",
+        "Tissue": "Kidney",
         "Dimension": "Offset",
         "Variable 1": 0.1003,
         "Variable 2": 0.003666,
         "Variable 3": 0
     }
 ]
+
+// TODO: multi probes are only available for Liver
+const MULTI_PROBES = [
+    {
+        "Probe": "PR",
+        "Count": 2,
+        "Length": 5.8,
+        "Height": 4.6,
+        "Offset": 0,
+    },
+    {
+        "Probe": "PR",
+        "Count": 3,
+        "Length": 6.5,
+        "Height": 5.3,
+        "Offset": 0
+    },
+    {
+        "Probe": "PR-XT",
+        "Count": 2,
+        "Length": 5.6,
+        "Height": 4.7,
+        "Offset": 0,
+    },
+    {
+        "Probe": "PR-XT",
+        "Count": 3,
+        "Length": 6.0,
+        "Height": 5.2,
+        "Offset": 0
+    },
+    {
+        "Probe": "PRS15",
+        "Count": 2,
+        "Length": 5.6,
+        "Height": 4.4,
+        "Offset": 0,
+    },
+    {
+        "Probe": "PRS15",
+        "Count": 3,
+        "Length": 6.2,
+        "Height": 4.9,
+        "Offset": 0
+    },
+    {
+        "Probe": "LK",
+        "Count": 2,
+        "Length": 6.4,
+        "Height": 4.8,
+        "Offset": 0,
+    },
+    {
+        "Probe": "LK",
+        "Count": 3,
+        "Length": 6.4,
+        "Height": 5.0,
+        "Offset": 0
+    },
+    {
+        "Probe": "LK-XT",
+        "Count": 2,
+        "Length": 6.8,
+        "Height": 4.9,
+        "Offset": 0,
+    },
+    {
+        "Probe": "LK-XT",
+        "Count": 3,
+        "Length": 6.5,
+        "Height": 5.3,
+        "Offset": 0
+    },
+    {
+        "Probe": "SR",
+        "Count": 2,
+        "Length": 6.6,
+        "Height": 5.1,
+        "Offset": 0,
+    },
+    {
+        "Probe": "SR",
+        "Count": 3,
+        "Length": 6.3,
+        "Height": 5.2,
+        "Offset": 0
+    },
+]
+
+const MAX_POWERS = {
+    'PR': 65,
+    'PR-XT': 65,
+    'LK': 140,
+    'LK-XT': 140,
+    'SR': 140,
+    'LN': 140,
+    'PRS15': 80
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,30 @@
 const visualDom = document.querySelector('.visual');
 const ellipseDom = document.querySelector('.ellipse');
-const probImg = document.querySelector('.probe-img');
+const probesContainerDom = document.querySelector('.probes-container');
+const probeDropdown = document.querySelector('.probe .dropdown-content');
 const infoModal = document.getElementById('info-modal');
 const splashModal = document.getElementById('splash-modal');
 const tissueOptions = document.querySelectorAll('.tissues button');
+const probeOptions = document.querySelectorAll('.probe .dropdown-content button');
 const probeQuantityOptions = document.querySelectorAll('.probe-quantity button');
-const probeSelectArrow = document.querySelector('.probe .arrow');
+const probeSelectArrow = document.querySelector('.probe .fa');
 
 // default values
-let tissue = 'LV';
-let probe = '1P';
+let tissue = 'Liver';
+let probe = 'PR';
 let time = 5;
 let power = 60;
 let probeQuantity = 1;
 let showProbeList = false;
+let MAX_POWER = 65;
 
 // static values
 const MIN_TIME = 1;
-const MAX_TIME = 6;
-const MIN_POWER = 35;
-const MAX_POWER = 65;
+const MAX_TIME = 10;
+const MIN_POWER = 5;
 const TIME_STEP = 1;
 const POWER_STEP = 5;
-const THRESHOLD = 4.6 + 0.6; // Max length + Max offset
+const THRESHOLD = 6.8; // Max length
 
 // modal
 function openInfoModal() {
@@ -42,12 +44,16 @@ window.onclick = function (event) {
     if (event.target == infoModal) {
         infoModal.style.display = "none";
     }
+
+    if (showProbeList) {
+        handleDropdown();
+    }
 }
 
 // mount event
 window.onload = function () {
     splashModal.style.display = "block";
-    visualDom.style.height = visualDom.clientWidth + "px";
+    disableProbes();
     renderEllipse();
     renderTime();
     renderPower();
@@ -63,7 +69,9 @@ tissueOptions.forEach(option => {
     option.addEventListener('click', function () {
         unselectAll(tissueOptions);
         option.className = "clicked";
-        tissue = option.getAttribute('value');
+        tissue = option.textContent;
+        disableProbes();
+        disableProbeQuantities();
         renderEllipse();
     });
 });
@@ -72,93 +80,149 @@ probeQuantityOptions.forEach(option => {
     option.addEventListener('click', function () {
         unselectAll(probeQuantityOptions);
         option.className = "clicked";
-        probeQuantity = option.getAttribute('value');
-
-        // TODO: option for now
-        // renderEllipse();
+        probeQuantity = parseInt(option.getAttribute('value'));
+        if (probeQuantity !== 1) {
+            document.querySelector('.time i').disabled = true;
+            document.querySelector('.power i').disabled = true;
+        }
+        disableTissues();
+        renderEllipse();
     });
 });
 
 function renderTime() {
     document.querySelector('.time .value').innerHTML = time + ':00';
-    document.querySelector('.time .step .up').disabled = time === MAX_TIME
-    document.querySelector('.time .step .down').disabled = time === MIN_TIME
+    document.querySelector('.time .chevron-up').disabled = time >= MAX_TIME
+    document.querySelector('.time .chevron-down').disabled = time <= MIN_TIME
 }
 
 function renderPower() {
     document.querySelector('.power .value').innerHTML = power;
-    document.querySelector('.power .step .up').disabled = power === MAX_POWER
-    document.querySelector('.power .step .down').disabled = power === MIN_POWER
+    document.querySelector('.power .chevron-up').disabled = power >= MAX_POWER
+    document.querySelector('.power .chevron-down').disabled = power <= MIN_POWER
 }
 
 function changeTime(direction) {
     time += TIME_STEP * direction;
     renderTime();
-    renderEllipse()
+    renderEllipse();
 }
 
 function changePower(direction) {
-    power += POWER_STEP * direction
+    power += POWER_STEP * direction;
     renderPower();
-    renderEllipse()
+    renderEllipse();
 }
 
 // probe type selector
 function changeProbe(value) {
     probe = value;
+    MAX_POWER = MAX_POWERS[value];
+    if (power > MAX_POWER) {
+        power = MAX_POWER;
+    }
+
+    renderPower();
+    disableTissues();
     renderEllipse();
 }
 
-function handleDropdown() {
+function handleDropdown(e) {
+    e?.stopPropagation();
+
     showProbeList = !showProbeList
-    document.querySelector('.probe .dropdown-content').style.display = showProbeList ? 'block' : 'none'
+    probeDropdown.style.display = showProbeList ? 'flex' : 'none'
     if (showProbeList) {
-        probeSelectArrow.classList.add('up')
-        probeSelectArrow.classList.remove('down')
+        probeSelectArrow.classList.add('fa-angle-up')
+        probeSelectArrow.classList.remove('fa-angle-down')
     } else {
-        probeSelectArrow.classList.add('down')
-        probeSelectArrow.classList.remove('up')
+        probeSelectArrow.classList.add('fa-angle-down')
+        probeSelectArrow.classList.remove('fa-angle-up')
     }
 }
 
-function renderEllipse() {
-    let Dimensions = { Length: 200, Height: 150, Offset: null };
-    const equations = Calculations.filter(({ Probe, Tissue }) => Probe === probe && Tissue === tissue)
+function disableProbes() {
+    const availableProbes = [...new Set(CALCULATIONS.filter(e => e.Tissue === tissue).map(e => e.Probe))];
 
-    probImg.setAttribute('src', `assets/img/probs/${probe}.png`);
+    probeOptions.forEach(probeOption => {
+        probeOption.disabled = false;
+        if (!availableProbes.includes(probeOption.textContent)) {
+            probeOption.disabled = true;
+        }
+    })
+}
+
+function disableProbeQuantities() {
+    probeQuantityOptions.forEach(option => {
+        option.disabled = false;
+        if (tissue !== 'Liver' && option.value != 1) {
+            option.disabled = true;
+        }
+    })
+}
+
+function disableTissues() {
+    let availableTissues = [...new Set(CALCULATIONS.filter(e => e.Probe === probe).map(e => e.Tissue))];
+    if (probeQuantity !== 1 && availableTissues.includes('Liver')) {
+        availableTissues = ['Liver']
+    }
+
+    tissueOptions.forEach(tissueOption => {
+        tissueOption.disabled = false;
+        if (!availableTissues.includes(tissueOption.textContent)) {
+            tissueOption.disabled = true;
+        }
+    });
+}
+
+function renderEllipse() {
+    visualDom.style.height = (document.querySelector('.container').clientWidth * (0.55 + probeQuantity / 10)) + "px";
+
+    let Dimensions = { Length: 200, Height: 150, Offset: null };
+    if (probeQuantity === 1) {
+        CALCULATIONS
+            .filter(({ Probe, Tissue }) => Probe === probe && Tissue === tissue)
+            .forEach(e => {
+                Dimensions[e.Dimension] = e['Variable 1'] * Math.log(time) + e['Variable 2'] * power + e['Variable 3']
+            })
+    } else {
+        Dimensions = MULTI_PROBES.filter(({ Count, Probe }) => Count === probeQuantity && Probe === probe)[0];
+    }
+
+    // render selected probe type
     document.querySelector('.probe-value').innerHTML = probe;
 
-    if (equations.length === 0) {
-        ellipseDom.style.display = "none"
-        probImg.style.marginRight = 0;
+    // render probe images 
+    probesContainerDom.innerHTML = '';
+    for (let i = 0; i < probeQuantity; i++) {
+        probesContainerDom.innerHTML += `<img alt="probe" src="assets/img/probs/${probe}.png" />`
+    }
+    probesContainerDom.style.marginRight = (visualDom.clientWidth * Dimensions.Offset / THRESHOLD) + 'px';
+
+    ellipseDom.style.display = "block"
+    ellipseDom.style.width = (visualDom.clientWidth * Dimensions.Length / THRESHOLD) + 'px';
+    ellipseDom.style.height = (visualDom.clientWidth * Dimensions.Height / THRESHOLD) + 'px';
+    ellipseDom.querySelector('.length-line .value').innerHTML = Dimensions.Length.toFixed(1) + 'cm'
+    ellipseDom.querySelector('.height-line .value').innerHTML = Dimensions.Height.toFixed(1) + 'cm'
+    ellipseDom.querySelector('.offset-line').style.visibility = "visible";
+    ellipseDom.querySelector('.offset-line .value').innerHTML = Dimensions.Offset.toFixed(1) + 'cm'
+    ellipseDom.querySelector('.offset-line').style.width = (visualDom.clientWidth * Math.abs(Dimensions.Offset) / THRESHOLD) + 'px';
+    ellipseDom.querySelector('.offset-line .len').style.width = (visualDom.clientWidth * Math.abs(Dimensions.Offset) / THRESHOLD) + 'px';
+    ellipseDom.querySelector('.offset-line').style.height = (visualDom.clientWidth * Dimensions.Height / (2 * THRESHOLD)) + 16 + 'px';
+
+    if (Dimensions.Offset < 0) {
+        ellipseDom.querySelector('.offset-line').style.right = (visualDom.clientWidth * Dimensions.Offset / THRESHOLD) + 'px';
+        ellipseDom.querySelector('.height-line').style.right = -((visualDom.clientWidth * Math.abs(Dimensions.Offset) / THRESHOLD) + 14) + 'px';
+        visualDom.style.marginRight = ((visualDom.clientWidth * Math.abs(Dimensions.Offset) / THRESHOLD) + 60) + 'px';
     }
     else {
-        ellipseDom.style.display = "block"
-        equations.forEach(e => {
-            Dimensions[e.Dimension] = e['Variable 1'] * Math.log(time) + e['Variable 2'] * power + e['Variable 3']
-        })
+        ellipseDom.querySelector('.offset-line').style.right = 0;
+        ellipseDom.querySelector('.height-line').style.right = '-14px';
+        visualDom.style.marginRight = '60px'
+    }
 
-        probImg.style.marginRight = (visualDom.clientWidth * Dimensions.Offset / THRESHOLD) + 'px';
-        ellipseDom.style.width = (visualDom.clientWidth * Dimensions.Length / THRESHOLD) + 'px';
-        ellipseDom.style.height = (visualDom.clientWidth * Dimensions.Height / THRESHOLD) + 'px';
-
-        ellipseDom.querySelector('.length-line .value').innerHTML = Dimensions.Length.toFixed(1) + 'cm'
-        ellipseDom.querySelector('.height-line .value').innerHTML = Dimensions.Height.toFixed(1) + 'cm'
-        ellipseDom.querySelector('.offset-line .value').innerHTML = Dimensions.Offset.toFixed(1) + 'cm'
-        ellipseDom.querySelector('.offset-line').style.width = (visualDom.clientWidth * Math.abs(Dimensions.Offset) / THRESHOLD) + 'px';
-        ellipseDom.querySelector('.offset-line .len').style.width = (visualDom.clientWidth * Math.abs(Dimensions.Offset) / THRESHOLD) + 'px';
-        ellipseDom.querySelector('.offset-line').style.height = (visualDom.clientWidth * Dimensions.Height / (2 * THRESHOLD)) + 16 + 'px';
-
-        if (Dimensions.Offset < 0) {
-            ellipseDom.querySelector('.offset-line').style.right = (visualDom.clientWidth * Dimensions.Offset / THRESHOLD) + 'px';
-            ellipseDom.querySelector('.height-line').style.right = -((visualDom.clientWidth * Math.abs(Dimensions.Offset) / THRESHOLD) + 14) + 'px';
-            visualDom.style.marginRight = ((visualDom.clientWidth * Math.abs(Dimensions.Offset) / THRESHOLD) + 40) + 'px';
-        }
-        else {
-            ellipseDom.querySelector('.offset-line').style.right = 0;
-            ellipseDom.querySelector('.height-line').style.right = '-14px';
-            visualDom.style.marginRight = '40px'
-        }
+    if (!Dimensions.Offset) {
+        ellipseDom.querySelector('.offset-line').style.visibility = "hidden";
     }
 }
 
